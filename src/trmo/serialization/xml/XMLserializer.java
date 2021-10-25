ed9907c80db83b0dc6576f575ca7cc3fb11d47be
TRMO <tromortensen@hotmail.com>
2021-10-25 20:17:07 +0200

    Refactoring to clean up printObject

--- src/trmo/serialization/xml/XMLserializer.java
+++ src/trmo/serialization/xml/XMLserializer.java
@@ -17,6 +17,9 @@ public class XMLserializer {
 
     private static void printObject(Object obj, StringBuilder sb, int indents) throws IllegalAccessException {
         Class<?> type = obj.getClass();
+
+        // TODO: 25/10/2021 wrapper types?
+
         if (type.isAssignableFrom(String.class)) {
             printSimple(obj, sb, indents);
 
@@ -24,48 +27,60 @@ public class XMLserializer {
             printSimple(obj, sb, indents);
 
         } else if (type.isArray()) {
-            sb.append(indents(indents)).append("<collection>").append("\n");
-            int length = Array.getLength(obj);
-            for (int i = 0; i < length; i++) {
-                sb.append(indents(indents + 1)).append("<element>").append("\n");
-                Object o = Array.get(obj, i);
-                if (obj.getClass().getComponentType().isPrimitive()) {
-                    printSimple(o, sb, indents + 2);
-                } else {
-                    printObject(o, sb, indents + 2);
-                }
-                sb.append(indents(indents + 1)).append("</element>").append("\n");
-            }
-            sb.append(indents(indents)).append("</collection>").append("\n");
+            printArray(obj, sb, indents);
 
         } else if (type.isEnum()) {
             sb.append("enum\n");
 
         } else if (Collection.class.isAssignableFrom(type)) {
-            sb.append(indents(indents)).append("<collection>").append("\n");
-            Collection<?> list = (Collection<?>) obj;
-            for (Object o : list) {
-                sb.append(indents(indents + 1)).append("<element>").append("\n");
-                printObject(o, sb, indents + 2);
-                sb.append(indents(indents + 1)).append("</element>").append("\n");
-            }
-            sb.append(indents(indents)).append("</collection>").append("\n");
+            printCollection((Collection<?>) obj, sb, indents);
 
         } else if (Object.class.isAssignableFrom(type)) {
-            sb.append(indents(indents)).append("<object type=\"" + type.getName() + "\">").append("\n");
-            Field[] fields = obj.getClass().getDeclaredFields();
-            for (Field field : fields) {
-                field.setAccessible(true);
-                Class<?> aClass = field.getType();
-                Object o = field.get(obj);
-                if (aClass.isPrimitive()) {
-                    printSimple(o, sb, indents + 1);
-                } else {
-                    printObject(o, sb, indents + 1);
-                }
-            }
-            sb.append(indents(indents)).append("</object>").append("\n");
+            printComplex(obj, sb, indents, type);
         }
+    }
+
+    private static void printComplex(Object obj, StringBuilder sb, int indents, Class<?> type) throws IllegalAccessException {
+        sb.append(indents(indents)).append("<object type=\"" + type.getName() + "\">").append("\n");
+        Field[] fields = obj.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            Class<?> aClass = field.getType();
+            Object o = field.get(obj);
+            if (aClass.isPrimitive()) {
+                printSimple(o, sb, indents + 1);
+            } else {
+                printObject(o, sb, indents + 1);
+            }
+        }
+        sb.append(indents(indents)).append("</object>").append("\n");
+    }
+
+    private static void printCollection(Collection<?> obj, StringBuilder sb, int indents) throws IllegalAccessException {
+        sb.append(indents(indents)).append("<collection>").append("\n");
+        Collection<?> list = obj;
+        for (Object o : list) {
+            sb.append(indents(indents + 1)).append("<element>").append("\n");
+            printObject(o, sb, indents + 2);
+            sb.append(indents(indents + 1)).append("</element>").append("\n");
+        }
+        sb.append(indents(indents)).append("</collection>").append("\n");
+    }
+
+    private static void printArray(Object obj, StringBuilder sb, int indents) throws IllegalAccessException {
+        sb.append(indents(indents)).append("<collection>").append("\n");
+        int length = Array.getLength(obj);
+        for (int i = 0; i < length; i++) {
+            sb.append(indents(indents + 1)).append("<element>").append("\n");
+            Object o = Array.get(obj, i);
+            if (obj.getClass().getComponentType().isPrimitive()) {
+                printSimple(o, sb, indents + 2);
+            } else {
+                printObject(o, sb, indents + 2);
+            }
+            sb.append(indents(indents + 1)).append("</element>").append("\n");
+        }
+        sb.append(indents(indents)).append("</collection>").append("\n");
     }
 
     private static void printSimple(Object obj, StringBuilder sb, int indents) {

6c9c0b3e1ab79b807f06e082b24a1d85da0e25ef
Troels Mortensen <tromortensen@hotmail.com>
2021-10-27 10:32:56 +0200

    Minor refactoring, no longer passing StringBUilder around, but instead making it a field of XMLCreator.

--- src/trmo/serialization/xml/ObjectCreator.java
+++ src/trmo/serialization/xml/ObjectCreator.java
@@ -1,10 +1,19 @@
 package trmo.serialization.xml;
 
+import org.xml.sax.*;
+
+
 class ObjectCreator<T> {
 
-    public T toObject(String xml, Class<T> type) {
-        xml = xml.replace("\n", "").replace("\t", "");
+    private String xml;
+    private Class<T> type;
 
+    public T toObject(String xml, Class<T> type) {
+        this.xml = xml;
+        this.type = type;
+        xml = xml.replace("\n", "").replace("\t", "");
+        int charIdx = 0;
+        //parse(0);
         return null;
     }
 }
--- src/trmo/serialization/xml/XMLCreator.java
+++ src/trmo/serialization/xml/XMLCreator.java
@@ -6,41 +6,43 @@ import java.util.Collection;
 
 class XMLCreator {
 
+    private StringBuilder sb;
+
     String toXML(Object obj) {
-        StringBuilder sb = new StringBuilder();
+        sb = new StringBuilder();
         int indents = 0;
-        addXMLDeclaration(sb);
-        printObject(obj, sb, indents);
+        addXMLDeclaration();
+        printObject(obj, indents);
 
         return sb.toString();
     }
 
-    private void printObject(Object obj, StringBuilder sb, int indents) {
+    private void printObject(Object obj, int indents) {
         Class<?> type = obj.getClass();
 
         // TODO: 25/10/2021 wrapper types?
 
         if (type.isAssignableFrom(String.class)) {
-            printSimple(obj, sb, indents);
+            printSimple(obj, indents);
 
         } else if (type.isPrimitive()) {
-            printSimple(obj, sb, indents);
+            printSimple(obj, indents);
 
         } else if (type.isArray()) {
-            printArray(obj, sb, indents);
+            printArray(obj, indents);
 
         } else if (type.isEnum()) {
             sb.append("enum\n");
 
         } else if (Collection.class.isAssignableFrom(type)) {
-            printCollection((Collection<?>) obj, sb, indents);
+            printCollection((Collection<?>) obj, indents);
 
         } else if (Object.class.isAssignableFrom(type)) {
-            printComplex(obj, sb, indents, type);
+            printComplex(obj, indents, type);
         }
     }
 
-    private void printComplex(Object obj, StringBuilder sb, int indents, Class<?> type) {
+    private void printComplex(Object obj, int indents, Class<?> type) {
         sb.append(indents(indents)).append("<object type=\"" + type.getName() + "\">").append("\n");
         Field[] fields = obj.getClass().getDeclaredFields();
         for (Field field : fields) {
@@ -53,47 +55,47 @@ class XMLCreator {
                 throw new IllegalStateException("Error accessing field " + field.getName() + " of object " + obj.getClass().getName() + ", message: " + e.getMessage());
             }
             if (aClass.isPrimitive()) {
-                printSimple(o, sb, indents + 1);
+                printSimple(o, indents + 1);
             } else {
-                printObject(o, sb, indents + 1);
+                printObject(o, indents + 1);
             }
         }
         sb.append(indents(indents)).append("</object>").append("\n");
     }
 
-    private void printCollection(Collection<?> objects, StringBuilder sb, int indents) {
+    private void printCollection(Collection<?> objects, int indents) {
         sb.append(indents(indents)).append("<collection>").append("\n");
         for (Object o : objects) {
             sb.append(indents(indents + 1)).append("<element>").append("\n");
-            printObject(o, sb, indents + 2);
+            printObject(o, indents + 2);
             sb.append(indents(indents + 1)).append("</element>").append("\n");
         }
         sb.append(indents(indents)).append("</collection>").append("\n");
     }
 
-    private void printArray(Object obj, StringBuilder sb, int indents) {
+    private void printArray(Object obj, int indents) {
         sb.append(indents(indents)).append("<collection>").append("\n");
         int length = Array.getLength(obj);
         for (int i = 0; i < length; i++) {
             sb.append(indents(indents + 1)).append("<element>").append("\n");
             Object o = Array.get(obj, i);
             if (obj.getClass().getComponentType().isPrimitive()) {
-                printSimple(o, sb, indents + 2);
+                printSimple(o, indents + 2);
             } else {
-                printObject(o, sb, indents + 2);
+                printObject(o, indents + 2);
             }
             sb.append(indents(indents + 1)).append("</element>").append("\n");
         }
         sb.append(indents(indents)).append("</collection>").append("\n");
     }
 
-    private void printSimple(Object obj, StringBuilder sb, int indents) {
+    private void printSimple(Object obj, int indents) {
         sb.append(indents(indents)).append("<value type=\"" + obj.getClass().getName() + "\">").append("\n");
         sb.append(indents(indents + 1)).append(obj).append("\n");
         sb.append(indents(indents)).append("</value>").append("\n");
     }
 
-    private void addXMLDeclaration(StringBuilder sb) {
+    private void addXMLDeclaration() {
         sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\" ?>").append("\n");
     }
 
